d8e9bedfd457059f5f7ef57e27f29693aca68280
Yanqin Jin <yanqin@fb.com>
2021-11-15 11:16:53 -0800

    Remove unused

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 # shellcheck disable=SC2148
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-export USE_HDFS=1
 export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server:$JAVA_HOME/jre/lib/amd64:$HADOOP_HOME/lib/native
 
 export CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath --glob`
